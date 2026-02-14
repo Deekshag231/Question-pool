59f328773daa6447fe2c1203d54fbbc9112b3d42
Deekshag231 <deekshagupta012yo@gmail.com>
2026-02-14 17:20:17 +0530

    Time: 0 ms (100%), Space: 45.2 MB (97.3%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,23 +14,20 @@
  * }
  */
 class Solution {
+    boolean diff=true;
     public boolean isBalanced(TreeNode root) {
-        return check(root) != -1;
+       helper(root);
+        return  diff;
     }
 
-    private int check(TreeNode node) {
-        if (node == null) return 0;
-
-        int left_h = check(node.left);
-        if (left_h == - 1) return -1;
-
-        int right_h = check(node.right);
-        if (right_h == -1) return -1;
-
-        if (Math.abs(left_h - right_h) > 1) {
-            return -1;
+    public int helper(TreeNode root){
+        if(root==null) return 0;
+        int lh=helper(root.left);
+        int rh=helper(root.right);
+        if(Math.abs(lh-rh)>1){
+            diff=false;
+            return 0;
         }
-
-        return Math.max(left_h, right_h) + 1;
+        return Math.max(lh,rh)+1;
     }
 }
