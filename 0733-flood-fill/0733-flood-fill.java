4da864bd694221aa72c59fc49253207c852269fb
Deekshag231 <deekshagupta012yo@gmail.com>
2026-03-23 19:03:58 +0530

    Time: 0 ms (100%), Space: 46.8 MB (57.78%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,12 +4,12 @@ class Solution {
         dfs(image,sr,sc,color,ans);
         return image;
     }
-    public void dfs(int[][] image,int sr,int sc,int color,int ans){
-        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||image[sr][sc]==color||image[sr][sc]!=ans){
+    public void dfs(int[][] image, int r,int c,int color,int ans){
+        if(r<0||c<0||r>=image.length||c>=image[0].length||image[r][c]!=ans||image[r][c]==color){
             return;
         }
-        image[sr][sc]=color;
-        int[][] dir={{sr,sc-1},{sr+1,sc},{sr,sc+1},{sr-1,sc}};
+        image[r][c]=color;
+        int[][] dir={{r,c+1},{r+1,c},{r-1,c},{r,c-1}};
         for(int[] e:dir){
             dfs(image,e[0],e[1],color,ans);
         }
