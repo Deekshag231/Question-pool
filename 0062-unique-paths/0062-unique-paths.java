b261e79d20415ecbe9d90645219afffdfbdd17b0
Deekshag231 <deekshagupta012yo@gmail.com>
2026-03-27 01:48:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (57.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,29 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
+        for(int[] i:dp){
+            Arrays.fill(i,-1);
         }
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
+        boolean[][] vis=new boolean[m][n];
+        int ans=helper(0,0,dp,vis);
+        return ans;
+    }
+    public static int helper(int r,int c,int[][] dp,boolean[][] vis){
+        if(r>dp.length-1||c>dp[0].length-1||vis[r][c]){
+            return 0;
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+        if(r==dp.length-1&&c==dp[0].length-1){
+            return 1;
         }
-        return dp[m-1][n-1];
+        if(dp[r][c]!=-1){
+            return dp[r][c];
+        }
+        vis[r][c]=true;
+        int ans=0;
+        ans+=helper(r+1,c,dp,vis);
+        ans+=helper(r,c+1,dp,vis);
+        vis[r][c]=false;
+        dp[r][c]=ans;
+        return dp[r][c];
     }
 }
