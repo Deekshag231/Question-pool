75a5eb9ec962c8c9d391ee02ebb913b0e498f52f
Deeksha Gupta <deekshagupta012yo@gmail.com>
2026-07-25 02:28:43 +0530

    Time: 1188 ms (56.26%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING min(sale_date)>='2019-01-01'
+and max(sale_date)<='2019-03-31';
